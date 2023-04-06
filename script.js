123aed0a5915eb107697b1820af5198723017fc3
Ukaegbu Gray <graynneji@outlook.com>
2023-04-06 21:34:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,170 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data) {
+  const html = `
+  <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.official}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+            <p class="country__row"><span>💰</span></p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
+// //Ajax call country 1
+// const getCountryAndNeigbour = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     //convert it from JSON to javascript object
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     //render country
+//     renderCountry(data);
+
+//     //Get neigbour country 2
+//     const [neigbour] = data.borders;
+
+//     if (!neigbour) return;
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neigbour}`);
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+
+//       renderCountry(data2, 'neigbour');
+//     });
+//   });
+// };
+
+// getCountryAndNeigbour('United states of america');
+// // getCountryAndNeigbour('nigeria');
+const getJSON = function (url, errorMsg = `Something went wrong`) {
+  fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+
+    return response.json();
+  });
+};
+
+// const request = fetch('https://restcountries.com/v3.1/name/usa');
+// console.log(request);
+
+// const getCountryData = function (country) {
+//   //country 1
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders[0];
+//       console.log(data[0]);
+
+//       if (!neighbour) return;
+
+//       //country 2
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err}`);
+//       renderError(`Something went wrong ${err.message} Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+const request = fetch('https://restcountries.com/v3.1/name/usa');
+console.log(request);
+
+const getCountryData = function (country) {
+  //country 1
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, `Country not found`)
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      console.log(data[0]);
+
+      if (!neighbour) throw new Error('No Neighbour found!');
+
+      //country 2
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        `Country not found`
+      );
+    })
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err}`);
+      renderError(`Something went wrong ${err.message} Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('usa');
+});
+
+// const requests = new XMLHttpRequest();
+
+// requests.open('GET', `https://api.openbrewerydb.org/breweries/new_york`);
+// requests.send();
+// console.log(requests);
+
+// requests.addEventListener('load', function () {
+//   //convert it from JSON to javascript object
+//   const datas = JSON.parse(this.responseText);
+//   console.log(datas);
+// });
+
+//using fetch -- promise
+const requests = fetch('https://api.openbrewerydb.org/breweries/')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data[0]);
+  });
+
+const breweries = function () {
+  fetch('https://api.openbrewerydb.org/breweries/')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data[0]);
+    });
+};
+breweries();
